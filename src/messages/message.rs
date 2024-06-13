43a918348e3e471cd8939a8d013350309cc22ae4
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2024-06-13 21:26:37 +0200

    version 0.1.0 ok for test

--- src/messages/message.rs
+++ src/messages/message.rs
@@ -15,3 +15,17 @@ impl Message {
     }
 }
 
+
+
+#[test]
+fn test_message() {
+    let msg = Message::BulkInRequestMessage(BulkInRequestMessage::new(5, 32, None));
+    let vec = msg.to_vec();
+    assert_eq!(vec, vec![
+        2, // Bulk-IN transfer
+        5, // b_tag
+        250, // b_tag_inverse
+        0,
+        32, 0, 0, 0, // transfer_size
+        0, 0, 0, 0] );
+}
