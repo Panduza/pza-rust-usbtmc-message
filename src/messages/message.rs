89d951916e9e36ea629d9e5bffad83e31779a870
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2024-06-13 20:52:48 +0200

    finalizing v1

--- src/messages/message.rs
+++ src/messages/message.rs
@@ -6,3 +6,12 @@ pub enum Message {
     BulkInRequestMessage(BulkInRequestMessage),
 }
 
+impl Message {
+    pub fn to_vec(&self) -> Vec<u8> {
+        match self {
+            Message::BulkOutRequestMessage(msg) => msg.to_vec(),
+            Message::BulkInRequestMessage(msg) => msg.to_vec(),
+        }
+    }
+}
+
